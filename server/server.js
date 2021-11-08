fd7822cc0929dd956fd7f36dc9081833884b95a7
Noah <noahm0217@gmail.com>
2021-11-08 14:39:52 -0700

    server.js/utils

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
+//dependencies
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//create middleware
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
+//server connection
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//creating server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+//importing type defs
+const { typeDefs, resolvers } = require('./schemas');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+server.applyMiddleware({app});
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
