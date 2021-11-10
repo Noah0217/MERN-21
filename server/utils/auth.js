68e400e87db76cf8b1dc91a169cdbc78a4d1c20b
Noah <noahm0217@gmail.com>
2021-11-10 13:49:45 -0700

    fixed auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,19 @@
-const jwt = require('jsonwebtoken');
+import decode from 'jwt-decode';
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+class AuthService {
+  // user profile
+  getProfile() {
+    return decode(this.getToken());
+  }
 
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // logged in / signed up
+  loggedIn() {
+    
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token); 
+  }
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+  
+}
 
-    if (!token) {
-      return req;
-    }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      //return req;
-    }
-    return req;
-    // send to next endpoint
-    //next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-};
+export default new AuthService();
